0940bfe16d3fe939792e5e6daaddfcb742cf874a
Rsja12 <asocarras10@gmail.com>
2020-09-08 15:02:45 -0400

    write override func tableView to return correct number of rows

--- image-viewer/ViewController.swift
+++ image-viewer/ViewController.swift
@@ -24,7 +24,11 @@ class ViewController: UITableViewController {
                 pictures.append(item)
             }
         }
-        print(pictures)
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
     }
 
 
