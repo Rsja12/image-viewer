2368b88099684c188c78b45a19c3c4ca6a6aa4be
Rsja12 <asocarras10@gmail.com>
2020-09-08 14:39:19 -0400

    make ViewController inherit from UITableViewController

--- image-viewer/ViewController.swift
+++ image-viewer/ViewController.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     
     var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
         let items = try! fm.contentsOfDirectory(atPath: path)
